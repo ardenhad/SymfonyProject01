09fcba0ed5df3cc2a3811f88c44431ccdb32da38
Arden Had <arden.haddeler@ozu.edu.tr>
2019-07-30 12:28:29 +0300

    Product page has been created. Product list(index) page has been updated listing-wise.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -39,5 +40,18 @@ class ProductController extends AbstractController
         return new Response($this->twig->render("product/index.html.twig", [
             "products" => $products
         ]));
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_product")
+     */
+    public function product(Product $product)
+    {
+        return new Response(
+            $this->twig->render(
+                "product/product.html.twig",
+                ["product" => $product]
+            )
+        );
     }
 }
