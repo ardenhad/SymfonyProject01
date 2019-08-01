a9951f6a353dcf1e974870fc61c373a18b1838c3
Arden Had <arden.haddeler@ozu.edu.tr>
2019-08-01 17:22:08 +0300

    Tiny bit fix to previous commit.

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 
 
 use App\Entity\User;
-use App\Form\UserType
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
